f721d2c22699d24826bac9d67cc7ab20bb1c9cca
AlexFly93 <muhabon93@gmail.com>
2022-12-09 13:07:42 +0200

    Add completed homework

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 "use strict";
+
+
+const user = {
+  id: 1,
+  privateInfo: {
+    fname: "Brad",
+    sname: "Pitt",
+    bday: {
+      day: 18,
+      month: 12,
+      year: 1963,
+    },
+    children: ["Helen", "Alex", "Georg", "Anna"],
+  },
+  contactInfo: {
+    phone: {
+      work: "123-12-45",
+      mobile: "005-002-003",
+    },
+    adress: {
+      town: "ZP",
+      street: "12Avenu",
+      house: 45,
+    },
+    mail: "brad@gmail.com",
+  },
+  profession: "actor",
+};
+
+const { privateInfo: { bday: { year: userBirthYear } } } = user
+console.log(userBirthYear)
+const { privateInfo: { children:[two]} } = user
+console.log(two);
+const { contactInfo: { phone: { work: userWorkPhone } } } = user
+console.log(userWorkPhone)
